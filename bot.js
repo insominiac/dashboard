668a277e4c6b5ab1fcd1dbfa9831abca000fe41e
Hemant D <hemant132us@gmail.com>
2023-02-08 15:17:29 +0530

    timeout incresed

--- bot.js
+++ bot.js
@@ -44,11 +44,11 @@ const submitForm = async (e) => {
     //   enable alert
     setTimeout(() => {
   document.querySelector(".alert").style.display = "block";
-    }, 10000);
+    }, 20000);
   //   remove the alert
   setTimeout(() => {
     document.querySelector(".alert").style.display = "none";
-  }, 11500);
+  }, 21500);
     document.getElementById('botform').reset();
    // console.log(name,des,downloadURL)
       }
